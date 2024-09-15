c2c7d33bda617db5d3f8ebf383876791afa3a8fa
purvang khonde <khonde.p@northeastern.edu>
2024-09-15 16:28:58 -0400

    Assignment 1 uploaded

---
+++ Assignment1/src/model/Product.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Product {
+    String Name;
+    String Description;
+    String Price;
+    String Availability;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String Price) {
+        this.Price = Price;
+    }
+
+    public String getAvailability() {
+        return Availability;
+    }
+
+    public void setAvailability(String Availability) {
+        this.Availability = Availability;
+    }
+}
