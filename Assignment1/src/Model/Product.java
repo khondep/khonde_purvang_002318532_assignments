7e71b9b7953b9e591be1327e7cd13dce90be23da
purvang khonde <khonde.p@northeastern.edu>
2024-09-19 23:49:39 -0400

    file uploaded

---
+++ Assignment1/src/Model/Product.java
@@ -0,0 +1,257 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Product {
+    String d_FirstName;
+    String d_LastName;
+    String Gender;
+    String Email;
+    String Phone;
+    String d_DriversLicenseNo;
+    String d_DriversLicenseState;
+    String Occupation;
+    String Organization;
+    String Education;
+    String SocialMediaLink;
+    String SSN;
+    String d_AddressLine1;
+    String d_AddressLine2;
+    String City;
+    String State;
+    String ZIP; 
+    String d_TelephoneNumber;
+    String d_FaxNumber;
+    String d_EmergencyContact;
+    public String getAddressLine2;
+
+    public String getD_FirstName() {
+        return d_FirstName;
+    }
+
+    public void setD_FirstName(String d_FirstName) {
+        this.d_FirstName = d_FirstName;
+    }
+
+    public String getD_LastName() {
+        return d_LastName;
+    }
+
+    public void setD_LastName(String d_LastName) {
+        this.d_LastName = d_LastName;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getD_DriversLicenseNo() {
+        return d_DriversLicenseNo;
+    }
+
+    public void setD_DriversLicenseNo(String d_DriversLicenseNo) {
+        this.d_DriversLicenseNo = d_DriversLicenseNo;
+    }
+
+    public String getD_DriversLicenseState() {
+        return d_DriversLicenseState;
+    }
+
+    public void setD_DriversLicenseState(String d_DriversLicenseState) {
+        this.d_DriversLicenseState = d_DriversLicenseState;
+    }
+
+    public String getOccupation() {
+        return Occupation;
+    }
+
+    public void setOccupation(String Occupation) {
+        this.Occupation = Occupation;
+    }
+
+    public String getOrganization() {
+        return Organization;
+    }
+
+    public void setOrganization(String Organization) {
+        this.Organization = Organization;
+    }
+
+    public String getEducation() {
+        return Education;
+    }
+
+    public void setEducation(String Education) {
+        this.Education = Education;
+    }
+
+    public String getSocialMediaLink() {
+        return SocialMediaLink;
+    }
+
+    public void setSocialMediaLink(String SocialMediaLink) {
+        this.SocialMediaLink = SocialMediaLink;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getD_AddressLine1() {
+        return d_AddressLine1;
+    }
+
+    public void setD_AddressLine1(String d_AddressLine1) {
+        this.d_AddressLine1 = d_AddressLine1;
+    }
+
+    public String getD_AddressLine2() {
+        return d_AddressLine2;
+    }
+
+    public void setD_AddressLine2(String d_AddressLine2) {
+        this.d_AddressLine2 = d_AddressLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getD_TelephoneNumber() {
+        return d_TelephoneNumber;
+    }
+
+    public void setD_TelephoneNumber(String d_TelephoneNumber) {
+        this.d_TelephoneNumber = d_TelephoneNumber;
+    }
+
+    public String getD_FaxNumber() {
+        return d_FaxNumber;
+    }
+
+    public void setD_FaxNumber(String d_FaxNumber) {
+        this.d_FaxNumber = d_FaxNumber;
+    }
+
+    public String getD_EmergencyContact() {
+        return d_EmergencyContact;
+    }
+
+    public void setD_EmergencyContact(String d_EmergencyContact) {
+        this.d_EmergencyContact = d_EmergencyContact;
+    }
+
+    public String getd_Firstname() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getd_lastname() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getgender() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String geteEmail() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getDriverLicenseno() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getdriverslicensestate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getEmergencyContact() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getFaxNo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getTelephone() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getcity() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getAddressLine1() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getsocialmedialink() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String geteducation() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getorganization() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getAddressLine2() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+   
+      
+    
+    
+}
